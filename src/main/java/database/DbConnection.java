0487b027b1ba78b25f23126246e4c46e059e5743
Lucas Suota <lucassuotass@gmail.com>
2024-04-11 22:11:39 -0300

    feat: inserting data

--- src/main/java/database/DbConnection.java
+++ src/main/java/database/DbConnection.java
@@ -2,13 +2,14 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DbConnection {
     public Connection connect() {
         Connection conn = null;
         try {
             Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "Bmw123bmw");
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/controledequalidade", "postgres", "Bmw123bmw");
             if(conn != null){
                 System.out.println("Connected to the database");
             } else {
@@ -21,5 +22,29 @@ public class DbConnection {
         }
 
         return conn;
+    }
+
+    public void createTable(Connection conn, String tableName){
+        Statement statement;
+        try{
+            String query="CREATE TABLE "+tableName+" (invoice INT PRIMARY KEY, plate VARCHAR(255))";
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table created successfully");
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void insertData(Connection conn, String tableName, int invoice, String plate){
+        Statement statement;
+        try{
+            String query="INSERT INTO "+tableName+" (invoice, plate) VALUES ("+invoice+", '"+plate+"')";
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data inserted successfully");
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,14 @@ package org.example;
 
 import database.DbConnection;
 import entities.Load;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         DbConnection db = new DbConnection();
-        db.connect();
+        Connection conn = db.connect();
+        db.insertData(conn, "controledequalidade", 323245, "ABC1234");
+
     }
 }
